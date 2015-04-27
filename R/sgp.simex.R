1bc254909fa6e261f1f25c982bc2cc2c29b1ac18
Adam VanIwaarden <avaniwaarden@gmail.com>
2015-04-26 23:50:05 -0600

    One more try with enviro

--- R/sgp.simex.R
+++ R/sgp.simex.R
@@ -225,15 +225,12 @@ simex.sgp <- function(
 					par.start <- startParallel(parallel.config, 'SIMEX')
 					if (toupper(parallel.config[["BACKEND"]]) == "FOREACH") {
 						mtx.subset <- simex.coef.matrices[[paste("qrmatrices", tail(tmp.gp,1), k, sep="_")]][[paste("lambda_", L, sep="")]] # Save on memory copying to R SNOW workers
-						environment(get.percentile.preds) <- emptyenv()
+						environment(get.percentile.preds) <- environment()
 						fitted[[paste("order_", k, sep="")]][which(lambda==L),] <- 
 							foreach(z=iter(sim.iters), .combine="+", .export=c('tmp.gp', 'k', 'taus'), 
 											.options.multicore=par.start$foreach.options) %dopar% { # .options.snow=par.start$foreach.options
 												as.vector(
-													get.percentile.preds(my.data=list(sqlite.db=tmp.dbname, b=z, k=k, taus=taus), isotonize=isotonize,
-# 														dbGetQuery(dbConnect(SQLite(), dbname = tmp.dbname),
-# 																paste("select ", paste(c("ID", paste('prior_', k:1, sep=""), "final_yr"), collapse=", ")," from simex_data where b in ('",z,"')", sep="")),
-															my.matrix = mtx.subset[[z]])/B)
+													get.percentile.preds(my.data=list(sqlite.db=tmp.dbname, b=z, k=k, taus=taus), my.matrix = mtx.subset[[z]], isotonize=isotonize)/B)
 							}
 					} else {
 						if (par.start$par.type == 'MULTICORE') {
